f97a1fed39c629d70c451434e69046a339b063ba
Pineapple <pineapple_cpp@163.com>
2021-09-04 14:24:35 +0800

    新增 HdfsWordCountDriver.java 在集群上运行

---
+++ src/main/java/com/pineapple/mapreduce/wordcount/HdfsWordCountDriver.java
@@ -0,0 +1,55 @@
+package com.pineapple.mapreduce.wordcount;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+public class HdfsWordCountDriver {
+
+    public static void main(String @NotNull [] args) throws IOException, InterruptedException, ClassNotFoundException {
+
+        // 创建一个新的 job
+        // 如果要 new 的 Configuration() 为空，则可以直接调用 getInstance()
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf);
+        job.setJarByClass(HdfsWordCountDriver.class);
+
+        // 指定一下 job 名
+        job.setJobName("HdfsWordCount");
+
+        // 关联 mapper 和 reducer
+        job.setMapperClass(WordCountMapper.class);
+        job.setReducerClass(WordCountReducer.class);
+
+        // 设置 map 输出的 kv 类型
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        // 设置最终输出的 kv 类型
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        // 判断输出路径是否存在，存在则删除
+        Path inputPath = new Path(args[0]);
+        Path outputPath = new Path(args[1]);
+        FileSystem fileSystem = FileSystem.get(conf);
+        if (fileSystem.exists(outputPath))
+            fileSystem.delete(outputPath, true);
+
+        // 设置输入路径和输出路径
+        FileInputFormat.setInputPaths(job, inputPath);
+        FileOutputFormat.setOutputPath(job, outputPath);
+
+        // 提交job
+        boolean result = job.waitForCompletion(true);
+        System.exit(result ? 0 : 1);
+    }
+}
